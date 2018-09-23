7911e04facff1df3befce5a4a668c55d51066fea
Akash <makash420@gmail.com>
2018-09-23 15:14:58 +0530

    while_loop

---
+++ loop/break/break_keywork.md
@@ -0,0 +1,7 @@
+Controlled exit!
+exiting the loop in a controlled manner
+the keyword `break` gives us the ability to exit out of while loops whenever we want
+while True:
+    command = input("Type exit to exit:")
+    if command == "exit":
+        break
---
+++ loop/while/copyme.py
@@ -0,0 +1,12 @@
+#WAP to fight with your sis!!
+
+
+#take user input
+msg=input("Hey how's it going \n")
+
+#loop
+while msg !="stop copying me":
+    print(msg)
+    msg=input()
+#instead of these 2 lines you could have msg=input(f"{msg} \n")
+print("Ok! I'll stop copying you")
