7561b41e9d14da95ed7ffc9ad7db97b191bf206f
Akash <makash420@gmail.com>
2018-09-23 14:02:59 +0530

    while_loop

---
+++ loop/while/exercise.py
@@ -0,0 +1,25 @@
+#😀
+#😀 😀
+#😀 😀 😀
+#😀 😀 😀 😀
+#😀 😀 😀 😀 😀
+#😀 😀 😀 😀 😀 😀
+#😀 😀 😀 😀 😀 😀 😀
+#😀 😀 😀 😀 😀 😀 😀 😀
+#😀 😀 😀 😀 😀 😀 😀 😀 😀
+#😀 😀 😀 😀 😀 😀 😀 😀 😀 😀
+#Print this
+#Using for loop
+for emoji in range(1,11):
+    print("\U0001f600 " * emoji)
+
+#Using while loop
+emoji=1
+while emoji < 11:
+    print("\U0001f600 " * emoji)
+    emoji +=1
+
+#Using nested loops
+for x in range(3):
+    for emoji in range(1,11):
+        print("\U0001f600 " * emoji)
